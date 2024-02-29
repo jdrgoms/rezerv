39ef24284b0595854ffd3e440a8aba494d07b194
Jader <jjadergomes@gmail.com>
2024-02-29 11:35:45 -0300

    feat: create main page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 import BookingProvider from '@/contexts/booking-context'
 
+import '@/styles/globals.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BookingProvider>
---
+++ src/pages/main.tsx
@@ -0,0 +1,5 @@
+import { BookingForm } from '@/components/booking-form'
+
+export default function Main() {
+  return <BookingForm />
+}
