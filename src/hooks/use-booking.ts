0a57b9764edf5fe3fce6f363779e595778e260c2
Jader <jjadergomes@gmail.com>
2024-02-29 20:46:11 -0300

    feat: create function to update booking

--- src/hooks/use-booking.ts
+++ src/hooks/use-booking.ts
@@ -18,5 +18,35 @@ export default function useBooking() {
     }
   }
 
-  return { createBooking }
+  const updateBooking = (updatedBooking: Booking) => {
+    try {
+      const storedBookings = getStorage()
+      const filteredBookings = storedBookings.map((booking) =>
+        booking.id === updatedBooking.id ? updatedBooking : booking,
+      )
+
+      setBookings(filteredBookings)
+      setStorage(filteredBookings)
+    } catch (error) {
+      console.error('Failed to update booking', error)
+    }
+  }
+
+  const deleteBooking = (bookingId: string) => {
+    console.log(bookingId)
+
+    try {
+      const storedBooking = getStorage()
+      const filteredBookings = storedBooking.filter(
+        ({ id }) => id !== bookingId,
+      )
+
+      setBookings(filteredBookings)
+      setStorage(filteredBookings)
+    } catch (err) {
+      console.error('Failed to remove booking:', err)
+    }
+  }
+
+  return { createBooking, updateBooking, deleteBooking }
 }
